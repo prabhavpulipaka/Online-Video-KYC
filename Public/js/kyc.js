35a8a3faca93772f06cf7570830a9f1a9076c2d7
RAGHAVA <97667550+raghava200@users.noreply.github.com>
2024-03-14 16:52:12 +0530

    Delete Public directory

--- Public/js/kyc.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById('submitButton');
-const overlay = document.getElementById('loading');
-
-btn.addEventListener('click', () => {
-    // overlay.classList.add('show');
-    overlay.classList += ' show';
-});
